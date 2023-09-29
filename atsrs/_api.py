ebd3d7208df59586b024b5cc62d7f096c931602b
Julian Berman <Julian@GrayVines.com>
2023-09-28 21:57:30 -0400

    WIP

--- atsrs/_api.py
+++ atsrs/_api.py
@@ -1,6 +1,27 @@
+from pathlib import Path
+
+from tree_sitter import Language, Parser
 import attr
 
 
+_PARENT = Path(__file__).parent
+SO_PATH = _PARENT / "build/atsree-sitter.so"
+TS_PATH = _PARENT.parent / "vendor/tree-sitter-typescript/typescript"
+
+
+def _parser():
+    Language.build_library(str(SO_PATH), [str(TS_PATH)])
+    parser = Parser()
+    parser.set_language(Language(str(SO_PATH), "typescript"))
+    return parser
+
+
+@attr.define()
 class Loader:
-    def one_from_string(self, string):
+
+    _parser = attr.field(factory=_parser)
+
+    def one_from_bytes(self, string):
+        tree = self._parser.parse(string)
+        import pudb; pu.db
         return attr.make_class("Alert", attrs={"error": attr.ib()})
--- atsrs/tests/test_atsrs.py
+++ atsrs/tests/test_atsrs.py
@@ -2,12 +2,25 @@ from atsrs import Loader
 
 
 def test_one_from_string():
-    Alert = Loader().one_from_string(
-        """
+    Alert = Loader().one_from_bytes(
+        b"""
         interface Alert {
           error: boolean;
         }
         """,
     )
 
-    Alert(error=False)
+    assert Alert(error=False).error == False  # noqa: E712
+
+
+def test_one_from_string_optional_attr():
+    Alert = Loader().one_from_bytes(
+        b"""
+        interface Alert {
+          error: boolean;
+          title?: boolean;
+        }
+        """,
+    )
+
+    assert Alert(error=True).title is None
